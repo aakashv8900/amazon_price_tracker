fc695ad80dcbd2b429b8c828a15f4bf5b22854ac
Aakash Verma <aakashv.8292@gmail.com>
2021-04-08 16:22:24 +0530

    Progress

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ import time
 
 URL = 'https://www.amazon.com/Tracfone-Apple-iPhone-Prepaid-Smartphone/dp/B08CL4CCG2/ref=sr_1_3?dchild=1&keywords=iphone&qid=1617786209&sr=8-3'
 headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64)', 'Cache-Control': 'no-cache', "Pragma": "no-cache"}
+desired_price = 195
 
 def price():
     page = requests.get(URL, headers=headers)
@@ -15,7 +16,7 @@ def price():
     converted_price = price[1:-3]
     converted_price = int(float(converted_price))
 
-    if(converted_price < 195):
+    if(converted_price > desired_price):
         send_mail()
 
     print(title.strip())
@@ -34,15 +35,19 @@ def send_mail():
 
     msg = f"Subject: {subject}\n\n{body}"
 
+    reciever = 'aakashv.1238900@gmail.com'
+
     server.sendmail(
         'aakashv.8292@gmail.com',
-        'aakashv.1238900@gmail.com',
+        reciever,
         msg
     )
     print('HEY MAIL HAS BEEN SENT!')
 
     server.quit()
 
-while(True):
-    price()
-    time.sleep(86400)
+price()
+
+#while(True):
+#    price()
+#    time.sleep(60)

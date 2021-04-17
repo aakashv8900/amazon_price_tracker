8040f0aa1c7a06b23e5acbf04d07cc54cff67682
Aakash Verma <aakashv.8292@gmail.com>
2021-04-17 13:44:12 +0530

    Travis

--- test/test.py
+++ test/test.py
@@ -1,3 +1,16 @@
+import pytest
+
+import app as flask_app
+
+@pytest.fixture
+def app():
+    yield flask_app
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
 def test_redirection(app,client):
     print(client)
     # checking redirection
